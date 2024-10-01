75b7242c76944ff842cf3bafb2309e3aa5636135
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-09-30 23:41:46 -0300

    Equals e Hashcode

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,22 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// equals = mais lento e resposta 100%
+		// HashCode = resposta rápida porém não é 100%
+		
+		String a = "Maria";
+		String b = "Alex";
+		
+		System.out.println(a.equals(b));	
+		// b = "Maria";
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode()); 
+		// Posso ter objetos diferentes mas pode ocorrer o mesmo resultado
+		// Regra de ouro do HashCode se os resultados são diferentes então os objetos são diferentes
+		
+		// Combinando os dois métodos HashCode para buscar os elementos iguais e depois usar o equals para ter a certeza que são iguais
+		
+		
 	}
 
 }
